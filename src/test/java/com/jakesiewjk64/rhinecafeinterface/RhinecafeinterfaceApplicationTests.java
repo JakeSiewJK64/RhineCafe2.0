7ba6a2105c4e42d8c7e8bb7734b863c8dc60498a
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-01 20:25:27 +0800

    update: added program files

---
+++ src/test/java/com/jakesiewjk64/rhinecafeinterface/RhinecafeinterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakesiewjk64.rhinecafeinterface;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhinecafeinterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
