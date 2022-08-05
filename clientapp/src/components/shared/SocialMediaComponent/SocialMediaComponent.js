7daf8a4d8b609e26b291e99df904db4ae486238f
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-05 21:42:46 +0800

    update: linkedin section

--- clientapp/src/components/shared/SocialMediaComponent/SocialMediaComponent.js
+++ clientapp/src/components/shared/SocialMediaComponent/SocialMediaComponent.js
@@ -1,12 +1,14 @@
 import caption from '../../../assets/img/caption.svg';
 import mail from '../../../assets/img/mail.svg';
 import github from '../../../assets/img/github.svg';
+import linkedin from '../../../assets/img/linkedin.svg';
 
 const SocialMediaComponent = () => {
 
     const images = [
         { imgUrl: mail, url: "mailto:joekanesiew@outlook.com" },
         { imgUrl: github, url: "https://github.com/JakeSiewJK64" },
+        { imgUrl: linkedin, url: "https://www.linkedin.com/in/jake-siew-joe-kane-a411811b5/" },
     ];
 
     const githubProf = "https://avatars.githubusercontent.com/u/47139291?v=4";
@@ -18,7 +20,7 @@ const SocialMediaComponent = () => {
             {
                 images.map(image => {
                     return (
-                        <a href={image.url} className="mt-1 mx-auto" style={{ "width": "30px" }}>
+                        <a href={image.url} className="mt-1 mx-auto" style={{ "width": "30px" }} target="_blank" rel="noreferrer">
                             <img src={image.imgUrl} alt="socialmedia" />
                         </a>
                     )
