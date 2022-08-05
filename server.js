cc7e00184a415afaae0ed00da40ac50f33754dfe
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-05 12:06:20 +0800

    update

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const path = require('path');
 const cors = require('cors');
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./conn');
 var app = express();
 const PORT = 5000;
 
 // declare routes
 const indexRouter = require("./routes/index");
+const experienceRouter = require("./routes/experience");
 
 // configuring middleware
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 app.use(cors())
 app.use(
     express.json({
@@ -15,11 +19,11 @@ app.use(
     })
 )
 
-// app.use(express.static("client/build"))
-
 // configuring routes
 app.use("/", indexRouter);
+// app.use("/experience", experienceRouter);
 
+// app.use(express.static("client/build"))
 // app.get("*", (req, res) => {
 //     res.sendFile(path.join(__dirname, "client/build/index.html"));
 // });
