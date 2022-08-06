f607c5bf365b9c21af3c9aaf24cd6ac93566c921
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-06 16:24:32 +0800

    update: education component

--- clientapp/src/components/Pages/EducationComponent/EducationComponent.js
+++ clientapp/src/components/Pages/EducationComponent/EducationComponent.js
@@ -1,10 +1,35 @@
 import './EducationComponent.css';
-import {} from '@mui/material';
+import { } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 const EducationComponent = () => {
+    const [educations, setEducation] = useState([]);
+
+    const getEducation = async () => {
+        const data = await fetch('/education/getEducation');
+        const response = await data.json();
+        setEducation(response);
+    }
+
+    useEffect(() => {
+        getEducation();
+    }, [])
+
     return (
         <>
-            Education
+            <div className='d-flex flex-column'>
+                {
+                    educations.map((education, index) => {
+                        return (
+                            <div className='my-1 d-flex flex-column'>
+                                <strong className='p-1 bg-black text-white' style={{ maxWidth: "300px" }} key={index}>{education.Institute}</strong>
+                                <div className='ms-5'>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </>
     )
 }
