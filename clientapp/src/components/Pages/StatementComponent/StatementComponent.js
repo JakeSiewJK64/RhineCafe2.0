38ce746d3902a6877dbc6f2b77f5aee1f181b38f
DESKTOP-TN9N4CC\Think <joekanesiew@outlook.com>
2022-08-06 16:24:46 +0800

    update: statement

--- clientapp/src/components/Pages/StatementComponent/StatementComponent.js
+++ clientapp/src/components/Pages/StatementComponent/StatementComponent.js
@@ -5,9 +5,11 @@ const StatementComponent = () => {
     return (
         <>
             <Typography>
-                Hi there,
-
-                Welcome to my LinkedIn profile! It is my goal to build a profession in data science and software/ mobile application engineering, I have a passion and interest towards Machine Learning and Deep Neural Networks and would like to know more about them and their applications. I hope to journey in this endeavor with you.
+                Hi there! My name is Jake Siew Joe Kane.
+                Welcome to my landing page! 
+                It is my goal to build a profession in data science and software/ mobile application engineering, 
+                I have a passion and interest towards Machine Learning and Deep Neural Networks and would like to
+                 know more about them and their applications. I hope to journey in this endeavor with you.
             </Typography>
         </>
     )
